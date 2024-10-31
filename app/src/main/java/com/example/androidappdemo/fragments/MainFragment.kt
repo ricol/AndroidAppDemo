44f0ef4493222b7bd0df330d3f80aeaea349f8f6
Ricol Wang <ricol.wang@ef.com>
2024-10-31 23:51:50 +0800

    update

--- app/src/main/java/com/example/androidappdemo/fragments/MainFragment.kt
+++ app/src/main/java/com/example/androidappdemo/fragments/MainFragment.kt
@@ -7,7 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import androidx.core.content.ContextCompat
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.androidappdemo.R
 
@@ -16,7 +16,7 @@ class MainFragment: Fragment() {
         val view = inflater.inflate(R.layout.fragment_main, container, false)
         Log.d(TAG, "$this -> view: $view")
         view.findViewById<ImageView>(R.id.imageView)?.setImageResource(R.drawable.ic_launcher_background)
-        view.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.blue_sky))
+        view.findViewById<TextView>(R.id.textView).text = (1..100).random().toString()
         return view
     }
 }
