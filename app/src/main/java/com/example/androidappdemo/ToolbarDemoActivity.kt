171b38c7795f3d361c15790c34434dfefe5d0fa5
Ricol Wang <ricol.wang@ef.com>
2024-11-05 12:23:45 +0800

    toolbar demo

--- app/src/main/java/com/example/androidappdemo/ToolbarDemoActivity.kt
+++ app/src/main/java/com/example/androidappdemo/ToolbarDemoActivity.kt
@@ -2,42 +2,32 @@ package com.example.androidappdemo
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.LinearLayout
 import android.widget.TextView
-import android.widget.Toolbar
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import androidx.fragment.app.FragmentContainerView
 
-class ToolbarDemoActivity: FragmentActivity() {
+class ToolbarDemoActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val layout = LinearLayout(this)
-        layout.orientation = LinearLayout.VERTICAL
-        val container = FragmentContainerView(this)
-        container.id = R.id.fragmentContainer
-        layout.addView(container)
-        val bottomLayout = LinearLayout(this)
-        bottomLayout.orientation = LinearLayout.HORIZONTAL
-        val btnFragment1 = Button(this)
-        btnFragment1.text = "Fragment 1"
-        btnFragment1.setOnClickListener {
-            supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, ToolbarFragment().apply { message = "Fragment 1" }).commit()
-        }
-        bottomLayout.addView(btnFragment1)
-        val btnFragment2 = Button(this)
-        btnFragment2.text = "Fragment 2"
-        btnFragment2.setOnClickListener {
-            supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, ToolbarFragment().apply { message = "Fragment 2" }).commit()
-        }
-        bottomLayout.addView(btnFragment2)
-        layout.addView(bottomLayout)
-        setContentView(layout)
+        setContentView(R.layout.activity_toolbar_demo)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        supportActionBar?.title = "ToolBar Demo"
+    }
 
-        val toolBar = Toolbar(this)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.fragment_menu, menu)
+        menu?.add("PlaceHolder")
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        Toast.makeText(this, "Menu clicked: ${item.title}", Toast.LENGTH_SHORT).show()
+        return super.onOptionsItemSelected(item)
     }
 }
 
