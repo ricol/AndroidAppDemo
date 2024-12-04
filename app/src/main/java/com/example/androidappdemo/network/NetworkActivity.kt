a64477f92d3683c38610da3aa69f8f6e665eb6fb
Ricol Wang <ricol.wang@ef.com>
2024-12-04 15:37:02 +0800

    RESTFul service demo

--- app/src/main/java/com/example/androidappdemo/network/NetworkActivity.kt
+++ app/src/main/java/com/example/androidappdemo/network/NetworkActivity.kt
@@ -3,6 +3,7 @@ package com.example.androidappdemo.network
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import com.example.androidappdemo.R
 import retrofit2.Call
@@ -22,16 +23,18 @@ class NetworkActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_network)
         findViewById<Button>(R.id.btnRESTService)?.setOnClickListener {
-            val retrofit = Retrofit.Builder().baseUrl("https://www.flickr.com").addConverterFactory(ScalarsConverterFactory.create()).build()
+            val retrofit = Retrofit.Builder().baseUrl("https://www.baidu.com").addConverterFactory(ScalarsConverterFactory.create()).build()
             val api = retrofit.create(FlickrAPI::class.java)
             val request = api.fetchContent()
             request.enqueue(object : Callback<String> {
                 override fun onResponse(call: Call<String>, response: Response<String>) {
                     Log.i("Response", "message: ${response.message()}, body: ${response.body()}")
+                    Toast.makeText(this@NetworkActivity, "success with body size: ${response.body().toString().count()}", Toast.LENGTH_LONG).show()
                 }
 
                 override fun onFailure(call: Call<String>, t: Throwable) {
                     Log.e("ERROR", "failure!")
+                    Toast.makeText(this@NetworkActivity, "failure with error: ${t}}", Toast.LENGTH_LONG).show()
                 }
             })
         }
